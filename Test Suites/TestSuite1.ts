3b2ac9e77b04a39ef5dfeceb7e9f93a6fc6246aa
Charafeddine.LAKHDAR <charafeddine.lakhdar@berger-levrault.com>
2022-09-27 12:22:28 +0100

    Changes

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6a5017f8-56ec-4633-ae17-c0297982ef3d</testSuiteGuid>
+   <testCaseLink>
+      <guid>bd048fd0-7445-4c7f-94b2-095d2371a1fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
